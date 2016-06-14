f1327601bbc84e732133b70ab61dd58cc9952965
asijzar <etharner@gmail.com>
2016-06-14 18:11:07 +1000

    Adding temporary data storing.

--- ice/apps.py
+++ ice/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
+from datetime import date
+from data import Data
 
 
 class IceConfig(AppConfig):
     name = 'ice'
+    date = date.today()
+    data = None
+
+    def receive_data(self):
+        print('Fetching data..')
+        self.data = Data()
+        print('Data received')
+
+    def ready(self):
+        if not self.data:
+            self.receive_data()
--- ice/views.py
+++ ice/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.apps import apps
 import json
 import datetime
 from ice.report.data import Data
@@ -8,14 +9,13 @@ import graph
 
 
 def prepare_response_data(method, sea_name):
-    data = Data()
+    app_data = apps.get_app_config('ice')
+    if datetime.datetime.today().date() > app_data.date:
+        app_data.receive_data()
 
-    if method == "source":
-        return data.parsed_data(sea_name)
-    if method == "mean":
-        return data.mean_data(sea_name)
-    if method == "normal":
-        return data.normal_data(sea_name)
+    data = app_data.data.sea_data
+
+    return data.sea_data[method][sea_name]
 
 
 def proceed_response(request, method, cols_loc, cols):
@@ -70,21 +70,22 @@ def normal(request):
     )
 
 
+rus_seas = {
+    'Берингово море': 'bering',
+    'Чукотское море': 'chukchi',
+    'Охотское море': 'okhotsk',
+    'Японское море': 'japan'
+}
+rus_prop = {
+    'Площадь': 'avg_area',
+    'Площадь с учетом сплоченности': 'avg_conc',
+    'Объем': 'avg_vol',
+}
+
+
 def correlation(request):
     if request.method == 'GET':
         if request.GET.get('action') == 'corr':
-            rus_seas = {
-                'Берингово море': 'bering',
-                'Чукотское море': 'chukchi',
-                'Охотское море': 'okhotsk',
-                'Японское море': 'japan'
-            }
-            rus_prop = {
-                'Площадь': 'avg_area',
-                'Площадь с учетом сплоченности': 'avg_conc',
-                'Объем': 'avg_vol',
-            }
-
             sea1 = rus_seas[request.GET.get('sea1')]
             sea2 = rus_seas[request.GET.get('sea2')]
             year = int(request.GET.get('year'))
@@ -92,9 +93,8 @@ def correlation(request):
             dec2 = int(request.GET.get('dec2'))
             prop = rus_prop[request.GET.get('prop')]
 
-            data = Data()
-            sea1_data = data.prep_data(sea1)
-            sea2_data = data.prep_data(sea2)
+            sea1_data = prepare_response_data('normal', sea1)
+            sea2_data = prepare_response_data('normal', sea2)
 
             try:
                 coeffs = Estimation.pirson_coeff(
@@ -120,7 +120,35 @@ def correlation(request):
         }
     )
 
+
 def get_src(request):
     file = open('ice/report/correlation/' + request.path.split('/')[-1], 'rb')
 
     return HttpResponse(content=file)
+
+
+def forecast(request):
+    if request.method == 'GET':
+        if request.GET.get('action') == 'forecast':
+            sea = rus_seas[request.GET.get('sea')]
+            year1 = int(request.GET.get('year1'))
+            dec1 = int(request.GET.get('dec1'))
+            year2 = int(request.GET.get('year2'))
+            dec2 = int(request.GET.get('dec2'))
+            prop = rus_prop[request.GET.get('prop')]
+
+            data = apps.get_app_config('ice').data.data_processing(sea, year1, dec1, year2, dec2, prop)
+            return HttpResponse(json.dumps({
+                'data': data,
+            }), content_type="application/json")
+
+    return render(
+        request,
+        'ice/forecast.html',
+        {
+            'data_type': 'Прогнозирование ледовой обстановки',
+            'start_years': range(1997, datetime.date.today().year + 1),
+            'end_years': range(1997, 2021),
+            'decs': range(1, 37)
+        }
+    )
